729e75739b9029eac0c8b108ca15faf491250061
Xiyuan <xliu1@tcd.ie>
2026-03-01 21:47:00 +0000

    Greek and Latin added for baptize

--- backend/converter.py
+++ backend/converter.py
@@ -4,8 +4,8 @@ from pathlib import Path
 # Resolve paths relative to this file, so it works no matter the CWD
 BASE_DIR = Path(__file__).resolve().parent  
 INPUT_DIR = BASE_DIR / "output"
-GERMANIC_PATH = INPUT_DIR / "parser_output_church_germanic.json"
-INDO_PATH = INPUT_DIR / "parser_output_church_indo.json"
+GERMANIC_PATH = INPUT_DIR / "parser_output_baptize_germanic.json"
+INDO_PATH = INPUT_DIR / "parser_output_baptize_indo.json"
 WORD = GERMANIC_PATH.stem.replace("parser_output_", "").replace("_germanic", "") # Infer WORD from parser input
 HIERARCHY_PATH = BASE_DIR / "languageHierarchy.json"
 EXPORT_BRANCHES = {
